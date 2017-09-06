ad2fe128934c572c2aa1614eb4272f1941fa0638
Franco Rezabek <rezabekf>
2017-09-07 00:27:06 +0100

    Use SNS to notify when portfolio is deployed

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -2,25 +2,35 @@ import io
 import zipfile
 import mimetypes
 import boto3
+from botocore.exceptions import ClientError
 from botocore.client import Config
 
+
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-1:886198463915:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.francorezabek.info')
-    build_bucket = s3.Bucket('portfoliobuild.francorezabek.info')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.francorezabek.info')
+        build_bucket = s3.Bucket('portfoliobuild.francorezabek.info')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print("Job Done!")
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print("Job Done!")
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully.")
+
+    except ClientError as error_message:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="{}".format(error_message))
+        raise
 
     return 'Hello from Lambda'
-    
